aa51bfc7b607e20b1f9e54f9f9aca7a6f7ee8f1f
Nicolai Tufar <ntufar@gmail.com>
2013-08-06 03:45:17 +0400

    o Using onValueChange() event as it changes more often o Using AsyncTask to perform the calculation and update the values in background in order to not slow down the UI

--- src/com/tufar/IPCalculator/MainActivity.java
+++ src/com/tufar/IPCalculator/MainActivity.java
@@ -4,10 +4,12 @@ import com.tufar.IPCalculator.R;
 
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.NumberPicker;
 import android.widget.NumberPicker.OnScrollListener;
+import android.widget.NumberPicker.OnValueChangeListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -42,41 +44,51 @@ public class MainActivity extends Activity {
         ip1.setMinValue(0);
         ip1.setMaxValue(255);
         ip1.setValue(192);
-        ip1.setOnScrollListener(updateTheResults);
+        ip1.setOnValueChangedListener(onValueChange);
         ip1.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
         ip2 = (NumberPicker)findViewById(R.id.ip2);
         ip2.setMinValue(0);
         ip2.setMaxValue(255);
         ip2.setValue(168);
-        ip2.setOnScrollListener(updateTheResults);
+        ip2.setOnValueChangedListener(onValueChange);
         ip2.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
         ip3 = (NumberPicker)findViewById(R.id.ip3);
         ip3.setMinValue(0);
         ip3.setMaxValue(255);
         ip3.setValue(0);
-        ip3.setOnScrollListener(updateTheResults);
+        ip3.setOnValueChangedListener(onValueChange);
         ip3.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
         ip4 = (NumberPicker)findViewById(R.id.ip4);
         ip4.setMinValue(0);
         ip4.setMaxValue(255);
         ip4.setValue(0);
-        ip4.setOnScrollListener(updateTheResults);
+        ip4.setOnValueChangedListener(onValueChange);
         ip4.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
         netmask_one_number = (NumberPicker)findViewById(R.id.netmask_one_number);
         netmask_one_number.setMinValue(8);
         netmask_one_number.setMaxValue(31);
         netmask_one_number.setValue(16);
-        netmask_one_number.setOnScrollListener(updateTheResults);
+        netmask_one_number.setOnValueChangedListener(onValueChange);
         netmask_one_number.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
     }
     
-    OnScrollListener updateTheResults = new OnScrollListener() {
+    OnValueChangeListener onValueChange = new OnValueChangeListener() {
 		@Override
-		public void onScrollStateChange(NumberPicker view, int scrollState) {
+		public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+			CalculateTheIP ctIP = new CalculateTheIP();
+			ctIP.execute(null,null,null);
+		}
+	};
+    
+	
+	private class CalculateTheIP extends AsyncTask<String, Integer, Long>{
+
+		@Override
+		protected Long doInBackground(String... params) {
 			Integer ip1i = ip1.getValue();
 			Integer ip2i = ip2.getValue();
 			Integer ip3i = ip3.getValue();
@@ -90,20 +102,23 @@ public class MainActivity extends Activity {
 							netmask_one_number_i.toString();
 			try{
 				ipv4 = new IPv4(CIDR);
-				subnetMaskExpanded.setText(ipv4.getNetmask());
-				numberOfHosts.setText(ipv4.getNumberOfHosts().toString());
-				broadcastAddress.setText(ipv4.getBroadcastAddress());
-				wildcardMask.setText(ipv4.getWildcardMask());
-				hostsAddressRange.setText(ipv4.getHostAddressRange());
-				netmaskBinary.setText(ipv4.getNetmaskInBinary());
 			}catch(Exception e){
 				subnetMaskExpanded.setText(e.getMessage());
 			}
+			return null;
 		}
-	};
-    
-    
 
+		@Override
+		protected void onPostExecute(Long result) {
+			subnetMaskExpanded.setText(ipv4.getNetmask());
+			numberOfHosts.setText(ipv4.getNumberOfHosts().toString());
+			broadcastAddress.setText(ipv4.getBroadcastAddress());
+			wildcardMask.setText(ipv4.getWildcardMask());
+			hostsAddressRange.setText(ipv4.getHostAddressRange());
+			netmaskBinary.setText(ipv4.getNetmaskInBinary());
+		}
+		
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
